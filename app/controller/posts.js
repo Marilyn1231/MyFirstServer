2e3fbcfe88156f4a3830344dc97da78a47fbb34b
qftgtr <qft.gtr@gmail.com>
2017-12-15 19:40:09 +0800

    remove unnecessary

--- app/controller/posts.js
+++ app/controller/posts.js
@@ -7,36 +7,6 @@ module.exports = app => {
 
       ctx.status = 200;
       ctx.body = { success: true };
-
-      // const rule = {
-      //   longitude: { type: 'number', required: false },
-      //   latitude: { type: 'number', required: false },
-      //   max: { type: 'number', required: false },
-      // };
-      // ctx.validate(rule);
-      //
-      // const { longitude, latitude, max } = ctx.query;
-      // if (longitude && latitude) {
-      //   // search around
-      //   ctx.body = await ctx.model.Posts
-      //     .find({ type: 'creation' })
-      //     .select('-data.data')
-      //     .where('loc')
-      //     .near({
-      //       center: {
-      //         type: 'Point',
-      //         coordinates: [ Number(longitude), Number(latitude) ],
-      //       },
-      //       maxDistance: Number(max) || 5000,
-      //     });
-      //   ctx.status = 200;
-      // } else {
-      //   // my posts
-      //   ctx.body = await ctx.model.Posts
-      //     .find({ userId: ctx.req.user.sub });
-      //     // .select('-data.data');
-      //   ctx.status = 200;
-      // }
     }
 
     async create(ctx) {
