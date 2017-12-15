08a68b7b9172be19369119ea3daaad3bae742fd5
qftgtr <qft.gtr@gmail.com>
2017-12-15 20:00:01 +0800

    /api/v1/positions

--- app/controller/positions.js
+++ app/controller/positions.js
@@ -2,6 +2,14 @@
 
 module.exports = app => {
   class PositionController extends app.Controller {
+    async index(ctx) {
+      ctx.status = 200;
+      ctx.body = {
+        success: true,
+        msg: 'This is from controller/posts/index',
+      };
+    }
+
     async create(ctx) {
       // POST /api/v1/positions
       const body = ctx.request.body;
--- app/router.js
+++ app/router.js
@@ -5,6 +5,8 @@ module.exports = app => {
   // app.resources('services', '/api', app.controller.services);
 
   app.resources('posts', '/api/v1/posts', app.controller.posts);
+  app.resources('positions', '/api/v1/positions', app.controller.positions);
+
   // app.resources('post/creations', '/api/v1/post/creations', app.controller.post.creations);
   // app.resources('post/brushes', '/api/v1/post/brushes', app.controller.post.brushes);
   //
