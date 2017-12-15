729481da8086a9fb4237fc635d151406d2296c9e
Marilyn1231 <marilyn_1231@163.com>
2017-12-15 19:16:52 +0800

    remove unnecessary codes

--- app/controller/posts.js
+++ app/controller/posts.js
@@ -4,35 +4,39 @@ module.exports = app => {
   class PostController extends app.Controller {
     async index(ctx) {
       // GET /api/v1/posts
-      const rule = {
-        longitude: { type: 'number', required: false },
-        latitude: { type: 'number', required: false },
-        max: { type: 'number', required: false },
-      };
-      ctx.validate(rule);
 
-      const { longitude, latitude, max } = ctx.query;
-      if (longitude && latitude) {
-        // search around
-        ctx.body = await ctx.model.Posts
-          .find({ type: 'creation' })
-          .select('-data.data')
-          .where('loc')
-          .near({
-            center: {
-              type: 'Point',
-              coordinates: [ Number(longitude), Number(latitude) ],
-            },
-            maxDistance: Number(max) || 5000,
-          });
-        ctx.status = 200;
-      } else {
-        // my posts
-        ctx.body = await ctx.model.Posts
-          .find({ userId: ctx.req.user.sub });
-          // .select('-data.data');
-        ctx.status = 200;
-      }
+      ctx.status = 200;
+      ctx.body = { success: true };
+
+      // const rule = {
+      //   longitude: { type: 'number', required: false },
+      //   latitude: { type: 'number', required: false },
+      //   max: { type: 'number', required: false },
+      // };
+      // ctx.validate(rule);
+      //
+      // const { longitude, latitude, max } = ctx.query;
+      // if (longitude && latitude) {
+      //   // search around
+      //   ctx.body = await ctx.model.Posts
+      //     .find({ type: 'creation' })
+      //     .select('-data.data')
+      //     .where('loc')
+      //     .near({
+      //       center: {
+      //         type: 'Point',
+      //         coordinates: [ Number(longitude), Number(latitude) ],
+      //       },
+      //       maxDistance: Number(max) || 5000,
+      //     });
+      //   ctx.status = 200;
+      // } else {
+      //   // my posts
+      //   ctx.body = await ctx.model.Posts
+      //     .find({ userId: ctx.req.user.sub });
+      //     // .select('-data.data');
+      //   ctx.status = 200;
+      // }
     }
 
     async create(ctx) {
--- config/config.default.js
+++ config/config.default.js
@@ -7,27 +7,27 @@ module.exports = appInfo => {
   config.keys = appInfo.name + '_1496065938215_2084';
 
   // add your config here
-  config.middleware = [ 'errorHandler', 'auth0' ];
+  config.middleware = [ 'errorHandler' ];
   config.errorHandler = { match: '/api' };
-  config.auth0 = {
-    match(ctx) {
-      const url = ctx.request.url;
-      if (url === '/api' || url.indexOf('/api?') === 0) {
-        return false;
-      }
-      if (url.indexOf('/api/v1/auth0/avatar') === 0) {
-        return false;
-      }
-      return true;
-    },
-  };
+  // config.auth0 = {
+  //   match(ctx) {
+  //     const url = ctx.request.url;
+  //     if (url === '/api' || url.indexOf('/api?') === 0) {
+  //       return false;
+  //     }
+  //     if (url.indexOf('/api/v1/auth0/avatar') === 0) {
+  //       return false;
+  //     }
+  //     return true;
+  //   },
+  // };
   // config.security = { csrf: { ignoreJSON: true } };
   config.security = { csrf: false };
 
-  config.mongoose = {
-    url: process.env.MONGO_URL,
-    options: { mongos: true },
-  };
+  // config.mongoose = {
+  //   url: process.env.MONGO_URL,
+  //   options: { mongos: true },
+  // };
 
   return config;
 };
